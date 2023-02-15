33a36f537cd1c776a71e6b569f80853ad9fb333d
abhi055 <abhiakbari023@gmail.com>
2023-02-15 11:49:44 +0100

    difference between class and entity

--- xmledit.py
+++ xmledit.py
@@ -1,22 +1,35 @@
 import xml.etree.ElementTree as ET
 import re
 
-xmlfile='Untitle.xml'
+xmlfile='class.xml'
 
 tree = ET.parse(xmlfile)
 root = tree.getroot()
 
 # ET.dump(tree)
 
-for ele in root.findall(".//mxCell[@value]"):
-    a = ele.attrib['value']
+# for ele in root.findall(".//mxCell[@value]"):
+
+#     a = ele.attrib['value']
    
-    if re.search('<span>',a):
+#     if re.search('<span>',a):
+#         b = re.search('>(.*)<',a)
+#         print(b.group(1))
+#     else:
+#         print(a)
+
+
+
+for ele in root.findall(".//mxCell[@value][@style]"):
+
+    shape = ele.attrib['style']
+    if re.search('ellipse',shape):
+        a = ele.attrib['value']
+        print(a)
+    elif re.search('rounded=0',shape):
+        a = ele.attrib['value']
         b = re.search('>(.*)<',a)
         print(b.group(1))
-    else:
-        print(a)
-
-
+    
 
 
